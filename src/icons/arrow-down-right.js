f4bf978067a8d8f6eed28df30ba0b0d524a865ca
gmarco <guillermo.marco@adoptaunabuelo.com>
2022-02-28 13:18:57 +0100

    first commit

---
+++ src/icons/arrow-down-right.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Svg,
+  Circle as _Circle,
+  Ellipse,
+  G,
+  LinearGradient,
+  RadialGradient,
+  Line,
+  Path,
+  Polygon,
+  Polyline,
+  Rect,
+  Symbol,
+  Text,
+  Use,
+  Defs,
+  Stop,
+} from 'react-native-svg';
+
+const ArrowDownRight = props => {
+  const { color, size, ...otherProps } = props;
+  return (
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...otherProps}
+    >
+      <Line
+        x1="7"
+        y1="7"
+        x2="17"
+        y2="17"
+        fill="none"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <Polyline
+        points="17 7 17 17 7 17"
+        fill="none"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+};
+
+ArrowDownRight.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+ArrowDownRight.defaultProps = {
+  color: 'black',
+  size: '24',
+};
+
+export default ArrowDownRight;
---
+++ src/icons/check-circle.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Svg,
+  Circle as _Circle,
+  Ellipse,
+  G,
+  LinearGradient,
+  RadialGradient,
+  Line,
+  Path,
+  Polygon,
+  Polyline,
+  Rect,
+  Symbol,
+  Text,
+  Use,
+  Defs,
+  Stop,
+} from 'react-native-svg';
+
+const CheckCircle = props => {
+  const { color, size, ...otherProps } = props;
+  return (
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...otherProps}
+    >
+      <Path
+        d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+        fill="none"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <Polyline
+        points="22 4 12 14.01 9 11.01"
+        fill="none"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+};
+
+CheckCircle.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+CheckCircle.defaultProps = {
+  color: 'black',
+  size: '24',
+};
+
+export default CheckCircle;
